82230d574d5247fba2841659b61bc8381aab77aa
Edfame <theedfame@gmail.com>
2020-01-15 12:11:56 +0000

    canal error script added

---
+++ tests/error_counter.py
@@ -0,0 +1,12 @@
+import sys
+
+with open(sys.argv[1], 'r') as file:
+    info = file.read()
+    error_counter = 0
+    for x in info:
+        if x == 'A':
+            error_counter += 1
+
+    print("File:", sys.argv[1],"\nErrors:", error_counter, "\nP(error):", error_counter/int(sys.argv[2]))
+
+
