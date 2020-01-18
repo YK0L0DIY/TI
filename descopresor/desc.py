90961e11e795efb6b1998b1b2549c3404ec0bc78
yk0l0diy <ana!11mor>
2020-01-18 09:26:22 +0000

    descompresor

---
+++ descopresor/desc.py
@@ -0,0 +1,40 @@
+import sys
+from struct import *
+
+input_file = sys.argv[1]
+
+# mudar para ser por stdin no futuro :D
+file = open(input_file, 'rb')
+# data = file.read()
+
+dictionary = {
+    0: 'broken-\n',
+    1: 'on-----\n',
+    2: 'off----\n',
+    3: 'unknown\n',
+}
+compressed_data = []
+
+while True:
+    rec = file.read(2)
+    if len(rec) != 2:
+        break
+    (data,) = unpack('>H', rec)
+    compressed_data.append(data)
+
+decompressed_data = ""
+string = ""
+next_code = len(dictionary)
+
+for code in compressed_data:
+
+    if not (code in dictionary):
+        dictionary[code] = string + (string[0])
+    decompressed_data += dictionary[code]
+    if not (len(string) == 0):
+        dictionary[next_code] = string + (dictionary[code])
+        next_code += 1
+    string = dictionary[code]
+
+print(decompressed_data)
+file.close()
